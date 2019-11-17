2801fe5e1aebc4c20a44bb2837ece0f00c628ffa
vmanav <zmanav.1999@gmail.com>
2019-11-17 22:36:17 +0530

    Made a seperate repository

---
+++ server.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const app = express();
+const ExpressPeerServer = require('peer').ExpressPeerServer;
+const http = require('http');
+const socket = require('socket.io');
+
+listOfUsers = [];
+
+// configuring Handlebars
+app.set('view engine', 'hbs');
+
+const PORT = 3000;
+
+const server = http.createServer(app)
+
+// configuring server for socket.IO
+const io = socket(server)
+
+server.listen(PORT, () => {
+    // console.log("Running on : http://localhost:3000")
+    console.log("p2pChat Running on : http://localhost:3000/p2pChat")
+})
+
+const options = {
+    debug: true
+}
+
+const peerserver = ExpressPeerServer(server, options);
+
+app.use('/', express.static(__dirname + '/scripts'))
+
+app.use('/p2pServer', peerserver);
+
+app.get('/p2p', (req, res) => {
+    res.render('temp.hbs', { users })
+})
+
+app.get('/p2pChat', (req, res) => {
+    res.render('index');
+})
+
+peerserver.on('connection', (client) => {
+    console.log("Client connected : ", client)
+    // users.push(client)
+    // console.log("Users Available : ", users)
+});
+
+
+
+io.on('connection', (socket) => {
+    console.log("Connection Established :", socket.id)
+
+    // console.log("Type of socket.id -->", typeof(socket.id)) <= string
+    // When the connection is made succesfully
+    socket.emit('connected')
+
+    socket.on('addUser', (data) => {
+        console.log(data)
+        let obj = {
+            username: data.username,
+            peerId: data.peerId
+        }
+        console.log("Object at serevr ->", obj)
+        listOfUsers.push(obj)
+        // usersockets[data.user] = socket.id
+        // // console.log(typeof (usersockets));   console.log(usersockets)
+
+
+        io.emit('alertAllAboutNewUser', {
+            list: listOfUsers
+        })
+    })
+
+    // socket.on("send_chat", (data) => {
+
+    //     if (data.recipient == null) {
+    //         // RCP is NULL, NORMAL MSSG
+    //         io.emit("recieve_chat", {
+    //             message: data.message,
+    //             username: data.username,
+    //             private: false
+    //         })
+
+    //     }
+    //     else {
+    //         // RCP is not NULL, PVT MSG
+    //         let rcpSocket = usersockets[data.recipient]
+
+    //         //  when no user exists for private mssg
+    //         if (typeof (rcpSocket) == "undefined") {
+
+    //             console.log("No such user Found")
+    //             io.to(usersockets[data.username]).emit("recieve_chat", {
+    //                 recipient: data.recipient
+    //             })
+    //             return;
+    //         }
+
+    //         // PRIVATE MSSG
+    //         io.to(rcpSocket).emit("recieve_chat", {
+    //             message: data.message,
+    //             username: data.username,
+    //             private: true
+    //         })
+    //     }
+    // })
+
+
+
+    socket.on('disconnect', (reason) => {
+        let username = null;
+        console.log('user disconnected, socketID : ', socket.id);
+
+        io.emit('jaRhahu', {
+            disconnect: true
+        })
+
+    });
+
+})
